065ef25a820029df5a8059863b2e740b28dfca0a
kiwakihiroto <human.k.kiwkai@gmail.com>
2023-11-21 17:12:01 +0900

    型の作成

--- AttendanceManage/src/main/java/com/example/AttendanceManage/controller/attendancesController.java
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/controller/attendancesController.java
@@ -1,4 +1,43 @@
 package com.example.AttendanceManage.controller;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 
-public class attendancesContorller {
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+@Controller
+public class attendancesController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @GetMapping("/")
+    public String address(Model model){
+        return "";
+    }
+
+    @GetMapping("/")
+    public String condition(Model model){
+        return "";
+    }
+    @GetMapping("/")
+    public String login(Model model){
+        return "";
+    }
+    @GetMapping("/")
+    public String place(Model model){
+        return "";
+    }
+    @GetMapping("/")
+    public String work(Model model) {
+        return "";
+    }
 }
