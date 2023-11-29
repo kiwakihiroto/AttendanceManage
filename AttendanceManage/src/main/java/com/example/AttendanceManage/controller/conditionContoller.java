4c82b88f1911491f7754b874eea369bddfde8d03
kiwakihiroto <human.k.kiwkai@gmail.com>
2023-11-29 11:54:19 +0900

    一旦完成

--- AttendanceManage/src/main/java/com/example/AttendanceManage/controller/conditionContoller.java
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/controller/conditionContoller.java
@@ -21,10 +21,14 @@ public class conditionContoller {
     private JdbcTemplate jdbcTemplate;
     @GetMapping("/condition")
     public String condition(Model model){
-//       データベース接続テスト
-        String sql = "SELECT * FROM attendances";
+        //       ユーザが所属する部署内にする必要があるが、ログインしたときのデータの持ち方で変わるため、分かり次第変更。
+        String sql = "select a.user_name, w.start_work, w.end_work, c.work_condition, p.work_place, a.tel, a.mail \n" +
+                "from attendances as a \n" +
+                "inner join work as w on a.login_id = w.login_id \n" +
+                "inner join condition as c on w.work_condition_id = c.work_condition_id \n" +
+                "inner join place as p on w.work_place_id = p.work_place_id";
         System.out.println(jdbcTemplate.queryForList(sql));
-//      一覧表示
+    //      一覧表示
         List<Map<String, Object>> attendanceData = jdbcTemplate.queryForList(sql);
         model.addAttribute("condition", attendanceData);
         return "condition";
