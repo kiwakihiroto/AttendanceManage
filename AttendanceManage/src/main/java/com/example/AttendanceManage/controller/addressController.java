a71dadecfc71cd0d8450588ae50c388a3cd91edd
kiwakihiroto <human.k.kiwkai@gmail.com>
2023-11-29 09:48:00 +0900

    コントローラー分割

---
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/controller/addressController.java
@@ -0,0 +1,28 @@
+package com.example.AttendanceManage.controller;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+@Controller
+public class addressController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @GetMapping("/address")
+    public String address(Model model){
+        return "";
+    }
+
+
+}
---
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/controller/conditionContoller.java
@@ -0,0 +1,32 @@
+package com.example.AttendanceManage.controller;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+@Controller
+public class conditionContoller {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @GetMapping("/condition")
+    public String condition(Model model){
+//       データベース接続テスト
+        String sql = "SELECT * FROM attendances";
+        System.out.println(jdbcTemplate.queryForList(sql));
+//      一覧表示
+        List<Map<String, Object>> attendanceData = jdbcTemplate.queryForList(sql);
+        model.addAttribute("condition", attendanceData);
+        return "condition";
+    }
+}
