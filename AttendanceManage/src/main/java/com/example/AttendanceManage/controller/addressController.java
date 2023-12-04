d7dd344fcddab0e931f3f0a8a3c605d6e937d711
KaidanRaito <aceriasun91@outlook.jp>
2023-12-04 11:51:51 +0900

    address修正　work進行中

--- AttendanceManage/src/main/java/com/example/AttendanceManage/controller/addressController.java
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/controller/addressController.java
@@ -24,21 +24,4 @@ public class addressController {
     public String address(Model model) {
         return "";
     }
-
-    @PostMapping("/work")
-    public String input_start_work(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 出勤ボタンを押したときに現在の時間を取得
-        Date nowDate = new Date();
-        SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm:ss");
-        String formatNowDate = sdf1.format(nowDate);
-        System.out.println(formatNowDate);
-
-        //  出勤時間をDBに追加
-        String sql = "insert into attendance (start_work) values ('" + formatNowDate + "')";
-        jdbcTemplate.update(sql);
-
-        //  勤怠状況一覧の表示
-        return "/attendanceList";
-
-    }
 }
