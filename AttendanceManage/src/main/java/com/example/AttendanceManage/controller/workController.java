f43119621ae5008be8b9c33c85a167b8ea6b2157
KaidanRaito <aceriasun91@outlook.jp>
2023-12-18 09:51:54 +0900

    work

--- AttendanceManage/src/main/java/com/example/AttendanceManage/controller/workController.java
+++ AttendanceManage/src/main/java/com/example/AttendanceManage/controller/workController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
