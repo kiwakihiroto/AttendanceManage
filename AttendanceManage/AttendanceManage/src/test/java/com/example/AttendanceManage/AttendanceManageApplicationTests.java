16518ff42ac08a7620be03a3602babe6a38f5a8c
zenchitozono <150783953+zenchitozono@users.noreply.github.com>
2024-01-16 17:11:23 +0900

    spanタグの追加

---
+++ AttendanceManage/AttendanceManage/src/test/java/com/example/AttendanceManage/AttendanceManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AttendanceManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
